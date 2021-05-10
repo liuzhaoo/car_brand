dbe63ed7995e51e6406049f12f5ea2a7850f98bd
zhaoliu <zhaoliu@deepglint.com>
2021-05-10 19:15:32 +0800

    增加计算方向和车型精度的功能

--- car_mid/infer.py
+++ car_mid/infer.py
@@ -27,15 +27,86 @@ from torch.utils.data._utils.collate import default_collate  # 注意版本
 
 #     return default_collate(batch_clips), default_collate(batch_targets), batch_keys
 
-lab_gt = '/home/zhaoliu/carbrand-master/carbrand-master/dict/all_cls_2_label_dict'
+mid_chexing = '/home/zhaoliu/car_brand/datasets_new/maps/mid_2_chexing'
+mid_label = '/home/zhaoliu/car_brand/datasets_new/maps/mid_2_label'
 
-lab_2_gt = {}
-for line in open(lab_gt,'r'):
+mid_2_chexing = {}
+mid_2_label = {}
+for line in open(mid_chexing,'r'):
     index,index1 = line.strip().split()
-    lab_2_gt[index1]=int(index)
+    mid_2_chexing[int(index)]=int(index1)
+
+for line in open(mid_label,'r'):
+    index,index1 = line.strip().split()
+    mid_2_label[int(index1)]=index
+
+def cal_chexing_acc(outputs, targets):
+    n_correct_elems = 0
+
+    assert len(outputs) == len(targets)
+    outputs = outputs.numpy()
+    outputs = np.argmax(outputs,1)
+
+    outputs = outputs.tolist()      
+    chexing_targets = targets.numpy().tolist()
+
+    chexing_targets = [mid_2_chexing[int(target)] for target in targets]  # int
+    chexing_outputs = [mid_2_chexing[int(output)] for output in outputs]  # int
 
 
 
+    for i in range(len(outputs)):
+
+        if str(chexing_outputs[i]) == str(chexing_targets[i]):
+            n_correct_elems+=1
+
+    return n_correct_elems/len(outputs)
+
+def cal_ori_acc(outputs, targets):
+    n_correct_elems = 0
+
+    assert len(outputs) == len(targets)
+    outputs = outputs.numpy()
+    outputs = np.argmax(outputs,1)
+
+    outputs = outputs.tolist()      
+    chexing_targets = targets.numpy().tolist()
+
+    ori_targets = []
+    ori_outputs = []
+
+    for target in chexing_targets:
+        ori_t = mid_2_label[target][0]
+        if ori_t == 'b':
+            ori = 0
+        elif ori_t == 's':
+            ori = 1
+        else:
+            ori =2
+        ori_targets.append(ori)
+
+    for output in outputs:
+        ori_o = mid_2_label[output][0]
+        if ori_o == 'b':
+            ori = 0
+        elif ori_o == 's':
+            ori = 1
+        else:
+            ori =2
+        ori_outputs.append(ori)
+
+
+    # ori_targets = [mid_2_chexing[int(target)] for target in targets]  # int
+    # ori_outputs = [mid_2_chexing[int(output)] for output in outputs]  # int
+
+
+
+    for i in range(len(outputs)):
+
+        if str(ori_targets[i]) == str(ori_outputs[i]):
+            n_correct_elems+=1
+
+    return n_correct_elems/len(outputs)
 
 def get_test_utils(test_path,keys_path):
     
@@ -74,6 +145,8 @@ def test(model,test_loader):
     batch_time = AverageMeter()
     data_time = AverageMeter()
     accuracies = AverageMeter()
+    accuracies_chexing = AverageMeter()
+    accuracies_ori = AverageMeter()
     end_time = time.time()
 
 
@@ -85,15 +158,18 @@ def test(model,test_loader):
             data_time.update(time.time() - end_time)
 
 
-            targets = targets.numpy().tolist()
-            targets = [lab_2_gt[str(target)] for target in targets]  # int
-            targets = torch.tensor(targets)
+
+            # chexing = torch.tensor(targets)
 
             outputs= model(inputs)
 
 
             acc = calculate_accuracy(outputs, targets)
+            acc_chexing = cal_chexing_acc(outputs,targets)
+            acc_ori = cal_ori_acc(outputs,targets)
 
+            accuracies_ori.update(acc_ori,inputs.size(0))
+            accuracies_chexing.update(acc_chexing,inputs.size(0))
             accuracies.update(acc, inputs.size(0))
 
             batch_time.update(time.time() - end_time)
@@ -103,11 +179,15 @@ def test(model,test_loader):
             print('test iter: {}/{}\t'
                       'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
                       'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
-                      'Acc {acc.val:.3f} ({acc.avg:.3f})'.format(
+                      'Acc_ori {acc_ori.val:.3f} ({acc_ori.avg:.3f})\t'
+                      'Acc_chexing {acc_chexing.val:.3f} ({acc_chexing.avg:.3f})\t'
+                      'Acc_mid {acc.val:.3f} ({acc.avg:.3f})'.format(
                     i + 1,
                     len(test_loader),
                     batch_time=batch_time,
                     data_time=data_time,
+                    acc_ori = accuracies_ori,
+                    acc_chexing=accuracies_chexing,
                     acc=accuracies))
 
 
@@ -126,18 +206,18 @@ def resume_model(pth_path, model):
 
 def main():
 
-    pth_path = '/home/zhaoliu/car_weight/car_full/results/train_weight2/save_37.pth'
+    pth_path = '/home/zhaoliu/car_brand/car_mid/results_newdata/random_sample/save_30.pth'
     
-    test_result=Path('/home/zhaoliu/car_class+ori/test_result')
+    # test_result=Path('/home/zhaoliu/car_class+ori/test_result')
     # test_path = '/home/zhaoliu/car_data/训练数据/4.9新加测试集/val_lmdb'
     # keys_path = '/home/zhaoliu/car_data/训练数据/4.9新加测试集/new_val.npy'
-    test_path = "/mnt/disk/zhaoliu_data/carlogo/lmdb/carlogo_test/allnewdata_img_test_lmdb"
-    keys_path = '/home/zhaoliu/car_data/keys_npy/cleaned_test.npy'
+    test_path = "/mnt/disk/zhaoliu_data/small_car_lmdb/train_val_lmdb"
+    keys_path = '/home/zhaoliu/car_brand/lmdb_data_new/val.npy'
 
 
-    model = resnet18(num_classes=27249)
+    model = resnet18(num_classes=1189)
     model = resume_model(pth_path,model)
-    test_loader = get_test_utils(test_path,test_result,keys_path)
+    test_loader = get_test_utils(test_path,keys_path)
     print('数据加载完毕...')
     test(model,test_loader)
 
